87c251dd030a1af925ce06e83a8bdb042d3db563
MonicaKang <mokaex@gmail.com>
2020-09-16 17:54:56 -0700

    update

--- ufo_level_1/static/js/ufo.js
+++ ufo_level_1/static/js/ufo.js
@@ -16,7 +16,39 @@ console.log(tableData);
 
 function dateFilter(sighting) {
     return sighting.datetime < "1/10/2010";
-}
+};
 var dateSighting = data.filter(dateFilter);
 console.log(dateSighting);
+
+var inputField = d3.select("#datetime");
+
+inputField.on("change", function() {
+    var newText = d3.event.target.value;
+
+ // console.log filtered input data   
+
+    function dateFilter(sighting) {
+        return sighting.datetime === newText;
+    }
+    var dateSighting = data.filter(dateFilter);
+    console.log(dateSighting);
+
+// displaying table of filtered input data
+
+    dateSighting.forEach(function(sightingData) {
+    console.log(sightingData);
+    var row = tbody.append("tr");
+
+    Object.entries(sightingData).forEach(function([key, value]) {
+        console.log(key, value);
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
+
+
+
+
+
+});
 
